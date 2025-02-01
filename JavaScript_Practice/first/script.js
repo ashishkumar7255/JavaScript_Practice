94b508592f59f5f0dbb37e13173f4fb157b4fea5
Ashish <ashishkr802162@gmail.com>
2025-02-01 16:10:34 +0530

    A button which change the text by click on it

---
+++ JavaScript_Practice/first/script.js
@@ -0,0 +1,5 @@
+var btn = document.querySelector("button");
+var newtext = document.querySelector("p");
+btn.addEventListener("click", function () {
+    newtext.textContent = "Select the first button on the page Select the first paragraph on the page Add a click event listener to the button When the button is clicked, clear the text inside the paragraph"
+})
